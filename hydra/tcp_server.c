dd33c2fb2e34295f7687c1cf56a08b4d0a306b31
warren fung <warrenfung10@gmail.com>
2018-03-03 19:36:28 -0800

    backwards, server to client msg

--- hydra/tcp_server.c
+++ hydra/tcp_server.c
@@ -12,59 +12,68 @@ int		main()
 {
 	char	server_message[256] = "Reached server!";
 
-	//create server socket
+	/*
+	 * create server socket
+	 */
 	int		server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-	//define server address
+	/*
+	 * define server address
+	 */
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(5000);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 
-	//bind the socket to specific ip + port
+	/*
+	 * bind the socket to specific ip + port
+	 */
 	printf("-----\nserver address defined\n-----\n");
 	int bind_status = bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 	if (bind_status < 0)
 		printf("bind error reason[%s]\n", strerror(errno));
 	else
 		printf("bind status [%i] success!\n", bind_status);
-	//listen
-	listen(server_socket, 5);	//5 is backlog number, max num of connections
+	/*
+	 * listen
+	 */
+	listen(server_socket, 5);	/*5 is backlog number, max num of connections */
 	
+	/*
+	 * create client socket
+	 */
 	int	client_socket;
 	client_socket = accept(server_socket, NULL, NULL);
 	
-	//send message
-	int bufsize = 1000;
-	char buf[bufsize + 1];
-	char custom_msg[1000];
+	/*
+	 * send message
+	 */
+	int msg_size = 1000;
+	char send_msg[msg_size + 1];
+	char *replace;
 	while (1)
 	{
-		bzero(custom_msg, 1000);
-		if (fgets(custom_msg, 1000, stdin))
+		bzero(send_msg, 1000);
+		if (fgets(send_msg, 1000, stdin))
 		{
-			char *replace = strchr(custom_msg, '\n');
+			replace = strchr(send_msg, '\n');
 			*replace = '\0';
-			printf("sending msg to client[%s]\n", custom_msg);
-	//	fgets(buf, 1000, stdin);
-		if (strcmp(custom_msg, "exit") == 0)
+			printf("sending msg to client[%s]\n", send_msg);
+		if (strcmp(send_msg, "exit") == 0)
 		{
 			send(client_socket, "exit", 4, 0);
 			printf("closing server properly\n");
 			close(server_socket);
 			exit(0);
 		}
-		if (strcmp(custom_msg, "ping") == 0)
+		if (strcmp(send_msg, "ping") == 0)
 			send(client_socket, "pong\npong\n", 10, 0);
 		else
-			send(client_socket, custom_msg, strlen(custom_msg), 0);
-		//send(client_socket, server_message, sizeof(server_message), 0);
-		//send(client_socket, server_message, sizeof(server_message), 0);
+			send(client_socket, send_msg, strlen(send_msg), 0);
 		}
 	}
 	printf("closing socket to server\n");
-	//close socket
 	close(server_socket);
 	return (0);
 }
